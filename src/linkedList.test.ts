8667c0ea156815c6bb378835886ac09dfbe81b7f
aymeric35 <Oshi2606@gmail.com>
2023-05-21 18:01:32 +0200

    🔥 Remove spyHelper and mocking

--- src/linkedList.test.ts
+++ src/linkedList.test.ts
@@ -1,22 +1,11 @@
 import LinkedList from './linkedList'
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect } from "vitest";
 
 const numberList = new LinkedList([84, 15, 54, 25, 60, 64, 47, 2]);
 const stringList = new LinkedList(['xajzo', 'dqzbtlox', 'vjod', 'uvvmxrrtcq', 'abyowrzgytv', 'rwzmnqj', 'qgi', 'hjdg']);
 const mixedList = new LinkedList([84, 15, 'dqzbtlox', 'vjod', 60, 'uvvmxrrtcq', 47, 'vkf']);
 
-const spyHelper = (
-  method: "append" | "prepend" | "size" | "head" | "tail" | "at" | "pop" | "contains" | "find" | "toString" | "insertAt" | "removeAt"
-) => {
-  return {
-    number: vi.spyOn(numberList, method),
-    string: vi.spyOn(stringList, method),
-    mixed: vi.spyOn(mixedList, method)
-  }
-}
-
 describe('append method', () => {
-  const spy = spyHelper('append')
   it('should append the value correctly', () => {
     numberList.append(1)
     expect(numberList.tail()?.value).toEqual(1)
@@ -27,17 +16,9 @@ describe('append method', () => {
     mixedList.append('test')
     expect(mixedList.tail()?.value).toEqual('test')
   })
-  it('should have been called once', () => {
-    expect(spy.number).toHaveBeenCalledOnce();
-    expect(spy.string).toHaveBeenCalledOnce();
-  })
-  it('should have been called twice', () => {
-    expect(spy.mixed).toHaveBeenCalledTimes(2);
-  })
 })
 
 describe('prepend method', () => {
-  const spy = spyHelper('prepend')
   it('should append the value correctly', () => {
     numberList.prepend(1)
     expect(numberList.head()?.value).toEqual(1)
@@ -48,13 +29,15 @@ describe('prepend method', () => {
     mixedList.prepend('test')
     expect(mixedList.head()?.value).toEqual('test')
   })
-  it('should have been called once', () => {
-    expect(spy.number).toHaveBeenCalledOnce();
-    expect(spy.string).toHaveBeenCalledOnce();
-  })
-  it('should have been called twice', () => {
-    expect(spy.mixed).toHaveBeenCalledTimes(2);
-  })
 })
+
+// describe('size method', () => {
+//   it('should return the correct size', () => {
+//     console.log(numberList.size());
+//     console.log(numberList.toString());
+    
+//     expect(numberList.size()).toEqual(10)
+//   })
+// })
 
 
