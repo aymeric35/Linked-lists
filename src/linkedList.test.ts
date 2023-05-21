1f93bb2a18dfdc1ada5a16f32455a4f800abe8a9
aymeric35 <Oshi2606@gmail.com>
2023-05-21 12:25:12 +0200

    ✅ Reduce samples sizes

--- src/linkedList.test.ts
+++ src/linkedList.test.ts
@@ -1,35 +1,13 @@
 import LinkedList from './linkedList'
 import { describe, it, expect, vi } from "vitest";
 
-const numberList = new LinkedList(
-  [
-    84, 15, 54, 25, 60, 64, 47, 2, 92, 60, 13, 76, 66, 77, 81,
-    89, 38, 40, 99, 62, 20, 79, 57, 24, 10, 12, 89, 48, 40, 30,
-    32, 96, 91, 38, 40, 25, 88, 35, 91, 40
-  ]
-);
-const stringList = new LinkedList(
-  [
-    'xajzo', 'dqzbtlox', 'vjod', 'uvvmxrrtcq', 'abyowrzgytv', 'rwzmnqj',
-    'qgi', 'hjdg', 'pchfkrcd', 'nften', 'szait', 'vkf', 'mcliwkjr',
-    'fuiajmmskkkb', 'vsrkflbx', 'evqhquzsvaca', 'gwnqwyv', 'aqnuymc',
-    'xygiusmn', 'bsrwzuwd'
-  ]
-);
-const mixedList = new LinkedList(
-  [
-    84, 15, 54, 25, 60, 64, 47, 2, 92, 60, 13, 76, 66, 77, 81,
-    'xajzo', 'dqzbtlox', 'vjod', 'uvvmxrrtcq', 'abyowrzgytv', 'rwzmnqj',
-    89, 38, 40, 99, 62, 20, 79, 57, 24, 10, 12, 89, 48, 40, 30,
-    'qgi', 'hjdg', 'pchfkrcd', 'nften', 'szait', 'vkf', 'mcliwkjr',
-    32, 96, 91, 38, 40, 25, 88, 35, 91, 40, 'fuiajmmskkkb', 'vsrkflbx',
-    'evqhquzsvaca', 'gwnqwyv', 'aqnuymc',
-  ]
-);
+const numberList = new LinkedList([84, 15, 54, 25, 60, 64, 47, 2]);
+const stringList = new LinkedList(['xajzo', 'dqzbtlox', 'vjod', 'uvvmxrrtcq', 'abyowrzgytv', 'rwzmnqj', 'qgi', 'hjdg']);
+const mixedList = new LinkedList([84, 15, 'dqzbtlox', 'vjod', 60, 'uvvmxrrtcq', 47, 'vkf']);
 
 const spyHelper = (
   method: "append" | "prepend" | "size" | "head" | "tail" | "at" | "pop" | "contains" | "find" | "toString" | "insertAt" | "removeAt"
-  ) => {
+) => {
   return {
     number: vi.spyOn(numberList, method),
     string: vi.spyOn(stringList, method),
