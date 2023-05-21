78d3e671abb7bf20b0ec59654fe0071f32d93079
aymeric35 <Oshi2606@gmail.com>
2023-05-21 18:07:59 +0200

    ✅ Divide append test in subtests

--- src/linkedList.test.ts
+++ src/linkedList.test.ts
@@ -6,11 +6,15 @@ const stringList = new LinkedList(['xajzo', 'dqzbtlox', 'vjod', 'uvvmxrrtcq', 'a
 const mixedList = new LinkedList([84, 15, 'dqzbtlox', 'vjod', 60, 'uvvmxrrtcq', 47, 'vkf']);
 
 describe('append method', () => {
-  it('should append the value correctly', () => {
+  it('should append the value correctly with a list composed of number values', () => {
     numberList.append(1)
     expect(numberList.tail()?.value).toEqual(1)
+  }),
+  it('should append the value correctly with a list composed of string values', () => {
     stringList.append('test')
     expect(stringList.tail()?.value).toEqual('test')
+  }),
+  it('should append the value correctly with a list composed of mixed values', () => {
     mixedList.append(1)
     expect(mixedList.tail()?.value).toEqual(1)
     mixedList.append('test')
