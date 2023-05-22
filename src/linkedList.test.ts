409e1af8f2d258e6962d43aa8e996a4b410eaf60
aymeric35 <Oshi2606@gmail.com>
2023-05-22 13:46:51 +0200

    ✅ Refactor lists initialization

--- src/linkedList.test.ts
+++ src/linkedList.test.ts
@@ -1,20 +1,27 @@
 import LinkedList from './linkedList'
 import { describe, it, expect } from "vitest";
 
-const numberList = new LinkedList([84, 15, 54, 25, 60, 64, 47, 2]);
-const stringList = new LinkedList(['xajzo', 'dqzbtlox', 'vjod', 'uvvmxrrtcq', 'abyowrzgytv', 'rwzmnqj', 'qgi', 'hjdg']);
-const mixedList = new LinkedList([84, 15, 'dqzbtlox', 'vjod', 60, 'uvvmxrrtcq', 47, 'vkf']);
+const initLists = () => {
+  const numberList = new LinkedList([84, 15, 54, 25, 60, 64, 47, 2]);
+  const stringList = new LinkedList(['xajzo', 'dqzbtlox', 'vjod']);
+  const mixedList = new LinkedList([84, 15, 'dqzbtlox', 'vkf']);
+
+  return {numberList, stringList, mixedList}
+}
+
 
 describe('tail method', () => {
   it('should return the tail of the Linked List', () => {
+    const {numberList, stringList, mixedList} = initLists()
     expect(numberList.tail()?.value).toEqual(2);
-    expect(stringList.tail()?.value).toEqual('hjdg');
+    expect(stringList.tail()?.value).toEqual('vjod');
     expect(mixedList.tail()?.value).toEqual('vkf');
   })
 })
 
 describe('head method', () => {
   it('should return the head of the Linked List', () => {
+    const {numberList, stringList, mixedList} = initLists()
     expect(numberList.head()?.value).toEqual(84);
     expect(stringList.head()?.value).toEqual('xajzo');
     expect(mixedList.head()?.value).toEqual(84);
@@ -23,14 +30,17 @@ describe('head method', () => {
 
 describe('append method', () => {
   it('should append the value correctly with a list composed of number values', () => {
+    const {numberList} = initLists()
     numberList.append(1)
     expect(numberList.tail()?.value).toEqual(1)
-  }),
+  });
   it('should append the value correctly with a list composed of string values', () => {
+    const {stringList} = initLists()
     stringList.append('test')
     expect(stringList.tail()?.value).toEqual('test')
-  }),
+  });
   it('should append the value correctly with a list composed of mixed values', () => {
+    const {mixedList} = initLists()
     mixedList.append(1)
     expect(mixedList.tail()?.value).toEqual(1)
     mixedList.append('test')
@@ -39,11 +49,18 @@ describe('append method', () => {
 })
 
 describe('prepend method', () => {
-  it('should append the value correctly', () => {
+  it('should append the value correctly with a list composed of number values', () => {
+    const {numberList} = initLists()
     numberList.prepend(1)
     expect(numberList.head()?.value).toEqual(1)
+  });
+  it('should append the value correctly with a list composed of string values', () => {
+    const {stringList} = initLists()
     stringList.prepend('test')
     expect(stringList.head()?.value).toEqual('test')
+  });
+  it('should append the value correctly with a list composed of mixed values', () => {
+    const {mixedList} = initLists()
     mixedList.prepend(1)
     expect(mixedList.head()?.value).toEqual(1)
     mixedList.prepend('test')
@@ -51,13 +68,13 @@ describe('prepend method', () => {
   })
 })
 
-// describe('size method', () => {
-//   it('should return the correct size', () => {
-//     console.log(numberList.size());
-//     console.log(numberList.toString());
-    
-//     expect(numberList.size()).toEqual(10)
-//   })
-// })
+describe('size method', () => {
+  it('should return the correct size', () => {
+    const {numberList, stringList, mixedList} = initLists()
+    expect(numberList.size()).toEqual(8)
+    expect(stringList.size()).toEqual(3)
+    expect(mixedList.size()).toEqual(4)
+  })
+})
 
 
